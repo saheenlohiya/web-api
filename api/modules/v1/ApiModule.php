85c619407b7d862fd72590293785073f7ab28394
DWAMIAN MCLEISH <dwamian@campgladiator.com>
2017-02-14 00:39:07 -0600

    - added api module.

---
+++ api/modules/v1/ApiModule.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\api\modules\v1;
+
+class ApiModule extends \yii\base\Module
+{
+    public function init()
+    {
+        parent::init();
+
+    }
+}
---
+++ api/modules/v1/traits/ActiveRecordDbConnectionTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\api\modules\v1\traits;
+
+trait ActiveRecordDbConnectionTrait
+{
+    public static function getDb()
+    {
+        return \Yii::$app->db;
+    }
+}
