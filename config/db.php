b8f8700d226162cd95c3dafd5b03d5da791d64d2
DWAMIAN MCLEISH <dwamian@campgladiator.com>
2017-04-22 22:09:42 -0500

    - added a specific tests db config.

--- config/db.php
+++ config/db.php
@@ -1,10 +1,19 @@
 <?php
-if(YII_ENV_DEV || YII_ENV == 'dev' || YII_ENV == 'test'){
+if(YII_ENV_DEV || YII_ENV == 'dev'){
     return [
         'class' => 'yii\db\Connection',
         'dsn' => 'mysql:host=tellus-main-cluster.cluster-cusw3pjt3qpv.us-east-1.rds.amazonaws.com;dbname=tellus_tests',
         'username' => 'tellus',
         'password' => 'Yux4JFTVdw6y7A7ZQs',
+        'charset' => 'utf8',
+    ];
+}
+else if(YII_ENV == 'test'){
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=127.0.0.1;dbname=tellus_tests',
+        'username' => 'ubuntu',
+        'password' => '',
         'charset' => 'utf8',
     ];
 }
@@ -17,4 +26,3 @@ else{
         'charset' => 'utf8',
     ];
 }
-
