8e6adb67ea42ead76b8d945434ec5521cc7d3fd1
DWAMIAN MCLEISH <dwamian@campgladiator.com>
2017-04-22 20:58:31 -0500

    - created an RDS Db to store info.

--- config/db.php
+++ config/db.php
@@ -2,18 +2,18 @@
 if(YII_ENV_DEV || YII_ENV == 'dev' || YII_ENV == 'test'){
     return [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=127.0.0.1;dbname=tellus_tests',
-        'username' => 'ubuntu',
-        'password' => '',
+        'dsn' => 'mysql:host=tellus-main-cluster.cluster-cusw3pjt3qpv.us-east-1.rds.amazonaws.com;dbname=tellus_tests',
+        'username' => 'tellus',
+        'password' => 'Yux4JFTVdw6y7A7ZQs',
         'charset' => 'utf8',
     ];
 }
 else{
     return [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=127.0.0.1;dbname=tellus',
-        'username' => 'root',
-        'password' => 'password',
+        'dsn' => 'mysql:host=tellus-main-cluster.cluster-cusw3pjt3qpv.us-east-1.rds.amazonaws.com;dbname=tellus',
+        'username' => 'tellus',
+        'password' => 'Yux4JFTVdw6y7A7ZQs',
         'charset' => 'utf8',
     ];
 }
