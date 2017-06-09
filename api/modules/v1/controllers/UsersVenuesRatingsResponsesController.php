a984d6c5ec03d5826f67305eddf7b29e5691180e
DWAMIAN MCLEISH <dwamian@campgladiator.com>
2017-06-08 22:44:36 -0500

    - added venue rating responses endpoints

---
+++ api/modules/v1/controllers/UsersVenuesRatingsResponsesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\api\modules\v1\controllers;
+
+use app\filters\TuQueryParamAuth;
+use app\models\UsersVenuesRatingsResponses;
+use yii\web\Response;
+
+class UsersVenuesRatingsResponsesController extends TuBaseApiController
+{
+    // We are using the regular web app modules:
+    public $modelClass = 'app\models\UsersVenuesRatingsResponses';
+
+    /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => TuQueryParamAuth::className(),
+            'except' => [],
+            'optional' => []
+        ];
+        return $behaviors;
+    }
+
+}
