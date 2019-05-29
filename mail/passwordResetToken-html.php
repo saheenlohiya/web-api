50fb8bdc70d081e49eb1fb4e994cf5ce8e20268a
junaidNadeem <junaid.pk48@gmail.com>
2019-05-30 03:37:46 +0500

    fix

--- mail/passwordResetToken-html.php
+++ mail/passwordResetToken-html.php
@@ -2,7 +2,7 @@
  
 use yii\helpers\Html;
  
-$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
+$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->resettoken]);
 ?>
  
 <div class="password-reset">
