ff96db9ebfabfe8d02caaef1acc4b0fc1a8532b8
junaidNadeem <junaid.pk48@gmail.com>
2019-05-30 02:22:43 +0500

    fix

--- views/site/forgot.php
+++ views/site/forgot.php
@@ -1,25 +1,40 @@
 <?php
-use yii\helpers\Html;
-use yii\widgets\ActiveForm;
-/* @var $this yii\web\View */
-/* @var $form yii\widgets\ActiveForm */
-/* @var $model \yii2mod\user\models\PasswordResetRequestForm */
-$this->title = Yii::t('yii2mod.user', 'Recover Password');
-$this->params['breadcrumbs'][] = $this->title;
+    use yii\helpers\Html;
+    use yii\widgets\ActiveForm;
 ?>
-<div class="site-request-password-reset">
-    <h1><?php echo Html::encode($this->title) ?></h1>
+<?php
+$this->title=Yii::$app->name . ' - Forgot Password';
+// $this->breadcrumbs=array(
+// 	'Forgot Password',
+// );
+?>
 
-    <p><?php echo Yii::t('yii2mod.user', 'Please fill out your email. A link to reset password will be sent there.'); ?></p>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
-            <?php echo $form->field($model, 'email'); ?>
-            <div class="form-group">
-                <?php echo Html::submitButton(Yii::t('yii2mod.user', 'Send'), ['class' => 'btn btn-primary']) ?>
-            </div>
-            <?php ActiveForm::end(); ?>
-        </div>
-    </div>
-</div>
+
+<div class="form">
+<div class="row">
+            Email : <input name="Lupa[email]" id="ContactForm_email" type="email">
+	</div>
+<?php 
+    $form = ActiveForm::begin([
+        'id' => 'forgot-form',
+        'enableClientValidation'=>true,
+        'validateOnSubmit' => true, // this is redundant because it true by default
+    ]);
+    
+    // ...
+    
+    
+    ?>
+
+	<div class="row">
+            Email : <input name="Lupa[email]" id="ContactForm_email" type="email">
+	</div>
+
+	<div class="row buttons">
+		<?php echo Html::submitButton('Submit'); ?>
+	</div>
+
+<?php ActiveForm::end(); ?>
+
+</div><!-- form -->
