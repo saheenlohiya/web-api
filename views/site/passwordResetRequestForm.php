df93d2e56a37ab7d68c68cb937b4328ac3444ae6
junaidNadeem <junaid.pk48@gmail.com>
2019-05-31 00:05:34 +0500

    fix

--- views/site/passwordResetRequestForm.php
+++ views/site/passwordResetRequestForm.php
@@ -6,16 +6,19 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Request password reset';
 ?>
  
-<div class="site-request-password-reset text-center">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <p>Please fill out your email. A link to reset password will be sent there.</p>
+<div class="site-request-password-reset">
+    <div style="margin:auto;">
+        <h1><?= Html::encode($this->title) ?></h1>
+        <p>Please fill out your email. A link to reset password will be sent there.</p>
+    </div>
+    
     <div class="row">
-        <div class="col-lg-6 offset-lg-3">
+        <div class="col-lg-5 center-align">
  
             <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
                 <?= $form->field($model, 'user_email')->textInput(['autofocus' => true]) ?>
                 <div class="form-group">
-                    <?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
+                    <?= Html::submitButton('Send', ['class' => 'btn btn-primary btn-lg float-right']) ?>
                 </div>
             <?php ActiveForm::end(); ?>
  
