9af7b687b64f4776b1e3c36272d3eae8443196cf
junaidNadeem <junaid.pk48@gmail.com>
2019-05-31 00:15:54 +0500

    fix

--- views/site/index.php
+++ views/site/index.php
@@ -6,15 +6,13 @@ $this->title = 'The TellUs App';
 ?>
 <div class="site-index">
 <?php if (Yii::$app->session->hasFlash('success')): ?>
-    <div class="alert alert-success alert-dismissable">
-         <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
-         <h4><i class="icon fa fa-check"></i>Saved!</h4>
+    <div class="alert alert-success">
+         <h4><i class="icon fa fa-check"></i>Success!</h4>
          <?= Yii::$app->session->getFlash('success') ?>
     </div>
 <?php endif; ?>
     <?php if (Yii::$app->session->hasFlash('error')): ?>
     <div class="alert alert-danger alert-dismissable">
-         <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
          <h4><i class="icon fa fa-check"></i>Error!</h4>
          <?= Yii::$app->session->getFlash('error') ?>
     </div>
--- views/site/resetPasswordForm.php
+++ views/site/resetPasswordForm.php
@@ -7,15 +7,18 @@ $this->title = 'Reset password';
 ?>
  
 <div class="site-reset-password">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <p>Please choose your new password:</p>
+    <div style="margin:auto;width:fit-content;margin-bottom:30px;">
+        <h1><?= Html::encode($this->title) ?></h1>
+        <p>Please choose your new password:</p>
+    </div>
     <div class="row">
-        <div class="col-lg-5">
+        <div class="col-lg-5 center-align">
  
             <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
                 <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'password_repeat')->passwordInput(['autofocus' => true]) ?>
                 <div class="form-group">
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary btn-lg float-right']) ?>
                 </div>
             <?php ActiveForm::end(); ?>
  
