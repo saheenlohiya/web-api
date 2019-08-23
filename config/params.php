00635d254f0248b6a6626386d440829edb486fc1
Andrew Dubinsky <adubinsky@gmail.com>
2019-08-22 21:14:37 -0500

    Fixing updated googleAPI password

--- config/params.php
+++ config/params.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'adminEmail' => ['dmcleish@gmail.com','Dbarthlow@yahoo.com','bobrocto@gmail.com','mckinney.john77@gmail.com'],
+    'adminEmail' => ['dmcleish@gmail.com','Dbarthlow@yahoo.com','bobrocto@gmail.com','mckinney.john77@gmail.com','tellusintel@gmail.com' ],
 //    'adminEmail' => ['dmcleish@gmail.com'],
     'customerServiceEmail' => 'support@thetellusapp.com',
-    'googleApiKey' => 'AIzaSyBNt6tgm-GSPdU4-sFtmD0o4mbckzTepVc',
+    'googleApiKey' => 'AIzaSyB-cWJkfUWPG9mOOitxHpIwL7Ww4FHE5Ac',
     'urbanairship'=>["key"=>"HCKFvIomS8q8fHD6gy0Cjg","secret"=>"KWJhz-cWRS6ObB9vCJlNEA"],
     'anonymousUserId'=>110,
     'user.passwordResetTokenExpire' => 3600,
